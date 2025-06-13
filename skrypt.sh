79b0d8ce7e2fa9bdac2967897ca505cb4c97218f
Mateusz Jurgielewicz <mateuszjurgielewicz@gmail.com>
2025-06-13 21:50:45 +0200

    Dodanie flagi --logs do tworzenia plików log

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+LOG_COUNT=100
+
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
   date
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  if [[ "$2" =~ ^[0-9]+$ ]]; then
+    LOG_COUNT=$2
+  fi
+  for ((i=1; i<=LOG_COUNT; i++)); do
+    filename="log${i}.txt"
+    echo -e "Nazwa pliku: $filename\nUtworzony przez: $0\nData: $(date)" > "$filename"
+  done
 else
   echo "Użyj --help lub -h, aby zobaczyć dostępne opcje."
 fi
