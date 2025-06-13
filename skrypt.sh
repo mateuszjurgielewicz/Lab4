68b9030f2218e098c8f57e9f7bf11756bdc4e983
Mateusz Jurgielewicz <mateuszjurgielewicz@gmail.com>
2025-06-13 21:54:31 +0200

    Dodanie flagi --init do klonowania repozytorium i ustawiania PATH

--- skrypt.sh
+++ skrypt.sh
@@ -9,7 +9,15 @@ show_help() {
   echo "--error | -e [n]  : tworzy katalogi error (domyślnie 100)"
 }
 
+REPO_URL="https://github.com/mateuszjurgielewicz/Lab4.git"
 LOG_COUNT=100
+
+init_repo() {
+  git clone "$REPO_URL"
+  REPO_DIR=$(basename "$REPO_URL" .git)
+  export PATH="$PATH:$(pwd)/$REPO_DIR"
+  echo "Repozytorium sklonowane i PATH ustawiony."
+}
 
 case "$1" in
   --date|-d)
@@ -26,6 +34,9 @@ case "$1" in
     ;;
   --help|-h)
     show_help
+    ;;
+  --init)
+    init_repo
     ;;
   *)
     echo "Nieznana opcja. Użyj --help lub -h."
